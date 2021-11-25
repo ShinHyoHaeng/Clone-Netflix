61fe8e5f87166487cb641afa09fe9e1b224bff69
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-11-25 23:58:43 +0900

    modify link

--- src/pages/Watch/Watch.js
+++ src/pages/Watch/Watch.js
@@ -24,7 +24,7 @@ const Watch = () => {
     return (
         <div className="watch">
             <div className="back">
-                <Link to="/home">
+                <Link to="/">
                     <ArrowBackOutlined/> 
                     Home
                 </Link>
